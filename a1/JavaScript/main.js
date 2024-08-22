eeaa1bef32fa3ee3b9a602f2301af16f8ca01f7e
s3933113 <s3933113@student.rmit.edu.au>
2024-08-23 04:12:11 +1000

    Full_version

--- a1/JavaScript/main.js
+++ a1/JavaScript/main.js
@@ -1,5 +1,6 @@
 document.querySelector('.dropdown').addEventListener('change', function() {
     var selectedOption = this.value;
+    console.log("Selected option: " + selectedOption);
     if (selectedOption) {
         location.href = selectedOption;
     }
--- a1/README.md
+++ a1/README.md
@@ -1,3 +1,3 @@
 # Assessment 1
 This is the file where you will have to provide a link to your project on the RMIT webserver
-
+https://titan.csit.rmit.edu.au/~s3933113/wp/a1/
