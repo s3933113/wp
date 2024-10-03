25f47a7195616498c1a4e88aab3e927190033bc4
s3933113 <s3933113@student.rmit.edu.au>
2024-10-04 08:17:36 +1000

    Edit 6 complete process_add.php to connect database

---
+++ a2/process_add.php
@@ -0,0 +1,33 @@
+<?php
+
+include('includes/db_connect.inc');
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $petName = $_POST['pet-name'];
+    $petType = $_POST['pet-type'];
+    $description = $_POST['description'];
+    $imageCaption = $_POST['image-caption'];
+    $age = $_POST['age'];
+    $location = $_POST['location'];
+
+    
+    $target_dir = "images/";
+    $target_file = $target_dir . basename($_FILES["pet-image"]["name"]);
+    move_uploaded_file($_FILES["pet-image"]["tmp_name"], $target_file);
+
+    
+    $stmt = $conn->prepare("INSERT INTO pets (petname, type, description, caption, age, location, image) VALUES (?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssssiss", $petName, $petType, $description, $imageCaption, $age, $location, basename($_FILES["pet-image"]["name"]));
+
+    if ($stmt->execute()) {
+        echo "New pet added successfully";
+        header("Location: pets.php");
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+?>
